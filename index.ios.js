3dfaf49f6df319fb4fcd2f2ab827ce7253d05961
Fernando Paredes <nano@fdp.io>
2016-08-21 17:01:03 -0700

    [iOS] Create navigation menu

--- index.ios.js
+++ index.ios.js
@@ -4,49 +4,52 @@
  * @flow
  */
 
+'use strict';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  NavigatorIOS,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-class PropertyFinder extends Component {
+class HelloWorld extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.text}>
+          Welcome to React Native (Again)!
         </Text>
       </View>
     );
   }
 }
 
+class PropertyFinder extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: HelloWorld,
+        }}
+      />
+    );
+  }
+}
+
 const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
