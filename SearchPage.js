3d52f036b98ad8627cd54be12f18de0b048465d3
Fernando Paredes <nano@fdp.io>
2016-08-21 21:05:36 -0700

    [SearchPage] Handle API responses

--- SearchPage.js
+++ SearchPage.js
@@ -96,11 +96,6 @@ class SearchPage extends Component {
     console.log(this.state.searchString);
   }
 
-  _executeQuery(query) {
-    console.log(query);
-    this.setState({ isLoading: true });
-  }
-
   onSearchPressed() {
     const query = urlForQueryAndPage('place_name', this.state.searchString, 1);
     this._executeQuery(query);
@@ -154,6 +149,35 @@ class SearchPage extends Component {
         </Text>
       </View>
     );
+  }
+
+
+  _executeQuery(query) {
+    console.log(query);
+    this.setState({ isLoading: true });
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+        this.setState({
+          isLoading: false,
+          message: 'Something bad happened ' + error,
+      }));
+  }
+
+  _handleResponse(response) {
+    this.setState({
+      isLoading: false,
+      message: '',
+    });
+
+    if (response.application_response_code.substr(0, 1) === '1') {
+      console.log('Properties found: ' + response.listings.length);
+    } else {
+      this.setState({
+        message: 'Location not recognized; please try again.',
+      })
+    }
   }
 }
 
