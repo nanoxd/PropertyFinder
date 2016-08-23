4477aa1355861449c6814d440be94c16729a6dcc
Fernando Paredes <nano@fdp.io>
2016-08-22 19:38:51 -0700

    [PropertyView] Render style

--- PropertyView.js
+++ PropertyView.js
@@ -40,3 +40,40 @@ const styles = StyleSheet.create({
     color: '#656565',
   }
 })
+
+class PropertyView extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const property = this.props.property
+    let stats = property.bedroom_number + ' bed ' + property.property_type
+
+    if (property.bathroom_number) {
+      stats += ', ' + property.bathroom_number + ' ' + (property.bathroom_number === 1 ? 'bathroom' : 'bathrooms')
+    }
+
+    const price = property.price_formatted.split(' ')[0]
+
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{uri: property.img_url}}
+        />
+        <View style={styles.heading}>
+          <Text style={styles.price}>£{price}</Text>
+          <Text style={styles.title}>{property.title}</Text>
+          <View style={styles.seperator}/>
+        </View>
+        <Text style={styles.description}>{stats}</Text>
+        <Text style={styles.description}>
+          {property.summary}
+        </Text>
+      </View>
+    )
+  }
+}
+
+module.exports = PropertyView;
