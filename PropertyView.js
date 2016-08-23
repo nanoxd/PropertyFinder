c90f2332861bd1e6a9caa7c4405c133592988ecd
Fernando Paredes <nano@fdp.io>
2016-08-22 19:30:48 -0700

    Add basic style for Property detail view

---
+++ PropertyView.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 65,
+  },
+  heading: {
+    backgroundColor: '#F8F8F8',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#DDDDDD',
+  },
+  image: {
+    width: 400,
+    height: 300,
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 5,
+    color: '#48BBEC',
+  },
+  title: {
+    fontSize: 20,
+    margin: 5,
+    color: '#656565',
+  },
+  description: {
+    fontSize: 18,
+    margin: 5,
+    color: '#656565',
+  }
+})
